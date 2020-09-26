12b7f3d2460b47ed68c3245650d2767b68d2e65c
draghn <g.vaSile@gmail.com>
2020-09-25 18:52:05 -0700

    Initial commit of the about me text

--- src/components/sections/aboutme/AboutMe.jsx
+++ src/components/sections/aboutme/AboutMe.jsx
@@ -7,27 +7,55 @@ export default () =>
         <Header>
             <SectionName>{'About Me'}</SectionName>
         </Header>
+        <AboutContainer>
+            <Idea>
+                I am passionate about technology and really enjoy working with other talented
+                people.
+            </Idea>
+            <Idea>
+                Learning quickly, I am thrilled to be part of a field that can move fast
+                and where I can constistely hone my skills and knowledge. Whether that is in a
+                particular programming language, or a new library, or getting more familiar with
+                a design pattern and how I can use it to improve products.
+            </Idea>
+        </AboutContainer>
     </Section>
 
 const Section = styled.section.attrs({
-    className: 'bg-center cover pl5-ns pr5-ns pb5'
+    className: 'cover pl5-ns pr5-ns pa7 flex items-center'
 })`
-    background-image:   linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
+    background-image:   linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0) 30%),
                         url(${backgrounds.about_large});
     background-attachment: fixed;
     
     @media only screen and (max-width: 767px) {        
-        background-image:   linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)),
-                            url(${backgrounds.home_small});
+        background-image:  url(${backgrounds.home_small});
+        flex-flow: column wrap;
     };
 `;
 
 const Header = styled.header.attrs({
-    className: 'flex'
+    className: 'ba bg-black-80'
 })``;
 
 const SectionName = styled.h1.attrs({
     className: 'f1 center'
 })`
-    color: rgb(255, 255, 255);
+    color: rgb(215, 215, 215);
+`;
+
+const AboutContainer = styled.div.attrs({
+    className: 'ba br3 white ml6 mr5 tc pl2 pr1 tracked'
+})`    
+    background: linear-gradient(to left, rgba(75, 85, 85, 1), rgba(10, 10, 10, 0.9) 20%);
+    
+    @media only screen and (max-width: 767px) {
+        margin-left: 1rem;
+        margin-right: 1rem;
+    };
+`
+const Idea = styled.p.attrs({
+    className: 'tl ln-copy light-silver bb'
+})`
+    color: rgb(205, 205, 215);
 `;
