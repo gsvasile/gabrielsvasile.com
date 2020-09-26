1609c8f70d02fcea262bfb96431cff6747e62229
draghn <g.vaSile@gmail.com>
2020-09-26 11:19:12 -0700

    Fixed issues seen on smaller screens.

--- src/components/sections/education/Education.jsx
+++ src/components/sections/education/Education.jsx
@@ -7,22 +7,24 @@ import ucscLogo from './ucsc-logo.png';
 export default () =>
     <Section id='education'>
         <Header>
-            <SectionName>{'Education'}</SectionName>
+            <SectionName>Education</SectionName>
         </Header>
         <EducationList />
     </Section>
 
 const Section = styled.section.attrs({
-    className: 'pl7-ns pr7-ns pb5'
+    className: 'pb5'
 })`
-    background: linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, 0)),
-                url(${ucscLogo}) no-repeat bottom right, 
-                url(${backgrounds.education_large}) no-repeat center fixed;
+    background: linear-gradient(rgba(0, 0, 0, .6), rgba(0, 0, 0, 0)),
+                url(${ucscLogo}) no-repeat bottom left, 
+                url(${backgrounds.education_large}) no-repeat  fixed;
     background-size: 1vh, 200px, cover;
+    padding-left: 8rem;
+    padding-right: 8rem;
 
     @media only screen and (max-width: 767px) {        
         background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)),
-                    url(${backgrounds.education_medium}) no-repeat center fixed;
+                    url(${backgrounds.home_small}) no-repeat center fixed;
         background-size: 1vh, cover;
         padding-left: 1rem;
         padding-right: 1rem;
@@ -36,7 +38,7 @@ const Header = styled.header.attrs({
 const SectionName = styled.h1.attrs({
     className: 'f1 center white'
 })`
-color: #rgb(15, 105, 200);
+    color: #rgb(15, 105, 200);
 `;
 
 
