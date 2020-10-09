e828049448af305a91a393d3caa741004413ab09
draghn <g.vaSile@gmail.com>
2020-10-09 09:20:16 -0700

    Updated a bullet on previous employment.

--- src/components/sections/experience/ExperienceList.jsx
+++ src/components/sections/experience/ExperienceList.jsx
@@ -53,7 +53,7 @@ const experienceList = [
         skills: ['PHP', 'SQLServer', 'MSAccess', 'VBA'],
         bullet1: '• Developed and maintained the in-house ERP software system using VBA, MS Access, and SQL Server.',
         bullet2: '• Renovated and enhanced the company website using HTML, PHP and SQL.',
-        bullet3: '• Led and trained a team to operate an Automated Optical Imaging machine that verifies surface-mounted circuits onto a PCB.',
+        bullet3: '• Led and trained a team to operate an Automated Optical Imaging machine that verifies surface-mounted circuits mounted onto a PCB.',
         bullet4: '• Assisted with turnkey quotes and material procurement for customers.',
     }
 ];
