7491a736bce97d7ab9f6fb8da38a6990177a30fb
draghn <g.vaSile@gmail.com>
2020-11-10 00:44:05 -0800

    Updating how the background images are imported

--- src/assets/backgrounds/backgrounds.js
+++ src/assets/backgrounds/backgrounds.js
@@ -1,24 +1,43 @@
+import home_large from './images/bg_home_2400x1600.jpg';
+import home_medium from './images/bg_home_1920x1280.jpg';
+import home_small from './images/bg_home_640x426.jpg';
+import experience_large from './images/bg_experience_2400x1597.jpg';
+import experience_medium from './images/bg_experience_1980x1277.jpg';
+import experience_small from './images/bg_experience_640x426.jpg';
+import portfolio_large from './images/bg_portfolio_2400x3200.jpg';
+import portfolio_medium from './images/bg_portfolio_1920x2560.jpg';
+import portfolio_small from './images/bg_portfolio_640x853.jpg';
+import skills_large from './images/bg_skills_1920x1440.jpg';
+import skills_medium from './images/bg_skills_1080x810.jpg';
+import skills_small from './images/bg_skills_640x440.jpg';
+import education_large from './images/bg_education_2700x1867.jpg';
+import education_medium from './images/bg_education_1301x751.jpg';
+import education_small from './images/bg_education_550x224.jpg';
+import about_large from './images/bg_about_2400x1600.jpg';
+import about_medium from './images/bg_about_1980x1280.jpg';
+import about_small from './images/bg_about_640x426.jpg';
+import small_width from './images/bg_small_width_640x320.jpg';
 
 const backgrounds = {
-    'home_large': require('./images/bg_home_2400x1600.jpg'),
-    'home_medium': require('./images/bg_home_1920x1280.jpg'),
-    'home_small': require('./images/bg_home_640x426.jpg'),
-    'experience_large': require('./images/bg_experience_2400x1597.jpg'),
-    'experience_medium': require('./images/bg_experience_1980x1277.jpg'),
-    'experience_small': require('./images/bg_experience_640x426.jpg'),
-    'portfolio_large': require('./images/bg_portfolio_2400x3200.jpg'),
-    'portfolio_medium': require('./images/bg_portfolio_1920x2560.jpg'),
-    'portfolio_small': require('./images/bg_portfolio_640x853.jpg'),
-    'skills_large': require('./images/bg_skills_1920x1440.jpg'),
-    'skills_medium': require('./images/bg_skills_1080x810.jpg'),
-    'skills_small': require('./images/bg_skills_640x440.jpg'),
-    'education_large': require('./images/bg_education_2700x1867.jpg'),
-    'education_medium': require('./images/bg_education_1301x751.jpg'),
-    'education_small': require('./images/bg_education_550x224.jpg'),
-    'about_large': require('./images/bg_about_2400x1600.jpg'),
-    'about_medium': require('./images/bg_about_1980x1280.jpg'),
-    'about_small': require('./images/bg_about_640x426.jpg'),
-    'small_width': require('./images/bg_small_width_640x320.jpg')
+    'home_large': home_large,
+    'home_medium': home_medium,
+    'home_small': home_small,
+    'experience_large': experience_large,
+    'experience_medium': experience_medium,
+    'experience_small': experience_small,
+    'portfolio_large': portfolio_large,
+    'portfolio_medium': portfolio_medium,
+    'portfolio_small': portfolio_small,
+    'skills_large': skills_large,
+    'skills_medium': skills_medium,
+    'skills_small': skills_small,
+    'education_large': education_large,
+    'education_medium': education_medium,
+    'education_small': education_small,
+    'about_large': about_large,
+    'about_medium': about_medium,
+    'about_small': about_small,
+    'small_width': small_width
 };
 
 export default backgrounds;
